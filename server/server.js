99d7ec55e7d6351ede74e06fc43bcd1b124f54b5
WEI TSE <curry900312@gmail.com>
2023-04-19 18:07:39 +0800

    defaultValue & filelimited solved

--- server/server.js
+++ server/server.js
@@ -9,7 +9,8 @@ dotenv.config()
 const app = express();
 
 /*Middleware*/
-app.use(express.json());
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb'}));
 app.use(cors());
 app.use(morgan("tiny"));
 app.disable("x-powered-by");
